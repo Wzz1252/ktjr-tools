4ff05b67a1b331d530ef6b3178e6b8db4a78d429
wangzhen <wangzhen951120@gamil.com>
2020-06-05 10:54:13 +0800

    添加失败重试机制

--- src/extraResources/MinShengBack.ts
+++ src/extraResources/MinShengBack.ts
@@ -22,7 +22,7 @@ const fs = require('fs');
 /** 民生银行支持的最大查询次数 */
 const QUERY_MAX_DAYS = 183;
 /** 重试间隔 */
-const RETRY_INTERVAL = 200; // ms
+const RETRY_INTERVAL = 100; // ms
 /** 重试次数 */
 const RETRY_NUMBER = 60; // ms
 /** 图片宽度 */
