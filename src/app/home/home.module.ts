f06b43c91af8e490086bbbf50270010b3800a2f9
wangzhen <wangzhen951120@gamil.com>
2020-06-10 14:33:11 +0800

    优化UI

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -7,9 +7,14 @@ import {HomeComponent} from './home.component';
 import {SharedModule} from '../shared/shared.module';
 import {HttpClientModule} from '@angular/common/http';
 
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+
 @NgModule({
-  declarations: [HomeComponent],
-  imports: [CommonModule, SharedModule, HomeRoutingModule, HttpClientModule]
+    declarations: [HomeComponent],
+    imports: [CommonModule, SharedModule, HomeRoutingModule, HttpClientModule,
+        MatButtonModule, MatInputModule, MatProgressSpinnerModule]
 })
 export class HomeModule {
 }
