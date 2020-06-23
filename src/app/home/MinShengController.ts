1f4dbe2894fd5b25dabe4fe928a97ce9e670fa9e
wangzhen <wangzhen951120@gamil.com>
2020-06-23 12:50:14 +0800

    添加一些极端的失败检查

--- src/app/home/MinShengController.ts
+++ src/app/home/MinShengController.ts
@@ -6,6 +6,8 @@ import YouXinImplTask from "../core/task/YouXinImplTask";
 import TaskCallbackListener from "../core/queue/TaskCallbackListener";
 import MinShengWebTask from "../core/task/MinShengWebTask";
 import PDFTask from "../core/task/PDFTask";
+import Logger from "../core/Logger";
+import TextUtils from "../core/TextUtils";
 
 const TAG = "MinShengController";
 /**
@@ -21,6 +23,8 @@ export default class MinShengController {
     private youxinCallback: TaskCallbackListener<MinShengEntity>;
     private webCallback: TaskCallbackListener<MinShengEntity>;
     private pdfCallback: TaskCallbackListener<MinShengEntity>;
+
+    private fileCallback: (code: string, errMsg: string) => void;
 
     public excelPath: string = "";
     public output: string = "";
@@ -96,26 +100,70 @@ export default class MinShengController {
         }
     }
 
-    /** 解析民生银行 */
+    /**
+     * 解析民生银行 Excel 文件
+     */
     public getMinShengBackXlsx(): Array<MinShengEntity> {
-        const workSheetsFromFile = XLSX.parse(this.excelPath);
         let xList: Array<MinShengEntity> = new Array<MinShengEntity>();
-        let item = workSheetsFromFile[0].data;
+
+        if (TextUtils.isEmpty(this.excelPath)) {
+            this.eventFail("-1", "Excel 文件路径不能为空");
+            return xList;
+        }
+
+        const workSheetsFromFile = XLSX.parse(this.excelPath);
+        const item = workSheetsFromFile[0].data || [];
+        if (item.length <= 0) {
+            this.eventFail("-1", "Excel 内容不能为空");
+            return xList;
+        }
+
+        function setValue(entity: MinShengEntity, value: string, key: string): boolean {
+            if (TextUtils.isEmpty(key)) {
+                return false;
+            }
+            entity[value] = key;
+            return true;
+        }
+
+        // 从 1 开始，忽略顶部的标签
         for (let i = 1; i < item.length; i++) {
             let entity = new MinShengEntity();
             entity.index = (i - 1);
-            entity.id = item[i][0] || "";
-            entity.loanDate = item[i][1] || "";
-            entity.startAdvanceDate = item[i][2] || "";
-            entity.endAdvanceDate = item[i][3] || "";
-            entity.productCode = item[i][4] || "";
-            entity.contractNo = item[i][5] || "";
-            entity.assetId = item[i][6] || "";
+            if (!setValue(entity, "id", item[i][0])) {
+                this.eventFail("-1", "Excel 第 " + i + " 行【身份证号码】不能为空");
+                return [];
+            }
+            if (!setValue(entity, "loanDate", item[i][1])) {
+                this.eventFail("-1", "Excel 第 " + (i + 1) + " 行【放款时间】不能为空");
+                return [];
+            }
+            if (!setValue(entity, "startAdvanceDate", item[i][2])) {
+                this.eventFail("-1", "Excel 第 " + (i + 1) + " 行【首次垫付时间】不能为空");
+                return [];
+            }
+            if (!setValue(entity, "endAdvanceDate", item[i][3])) {
+                this.eventFail("-1", "Excel 第 " + (i + 1) + " 行【末次垫付时间】不能为空");
+                return [];
+            }
+            if (!setValue(entity, "productCode", item[i][4])) {
+                this.eventFail("-1", "Excel 第 " + (i + 1) + " 行【理财端借款标的ID】不能为空");
+                return [];
+            }
+            if (!setValue(entity, "contractNo", item[i][5])) {
+                this.eventFail("-1", "Excel 第 " + (i + 1) + " 行【进件号】不能为空");
+                return [];
+            }
+            if (!setValue(entity, "assetId", item[i][6])) {
+                this.eventFail("-1", "Excel 第 " + (i + 1) + " 行【存管账户号】不能为空");
+                return [];
+            }
             entity.status = "WAIT";
             entity.output = this.output;
             entity.waitTime = this.waitTime;
             xList.push(entity);
         }
+
         this.minShengList = xList;
         return xList;
     }
@@ -130,6 +178,10 @@ export default class MinShengController {
 
     public setPdfCallback(callback: TaskCallbackListener<MinShengEntity>): void {
         this.pdfCallback = callback;
+    }
+
+    public setFailCallback(failCallback: (code: string, errMsg: string) => void): void {
+        this.fileCallback = failCallback;
     }
 
     private setListener(): void {
@@ -155,6 +207,12 @@ export default class MinShengController {
             this.pdfQueue.setCallback(((statue: any, data: MinShengEntity) => {
                 if (this.pdfCallback) this.pdfCallback(statue, data);
             }));
+        }
+    }
+
+    private eventFail(code: string, errMsg: string) {
+        if (this.fileCallback) {
+            this.fileCallback(code, errMsg);
         }
     }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,7 +2,7 @@ import {ChangeDetectorRef, Component, NgZone, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
 import MinShengEntity from "../entity/MinShengEntity";
 import MinShengController from "./MinShengController";
-import Logger from "../core/Logger";
+import TextUtils from "../core/TextUtils";
 
 const TAG = "HomeComponent";
 
@@ -15,7 +15,6 @@ export class HomeComponent implements OnInit {
     public static WEB_WAIT_ITEM: string = "WEB_WAIT_ITEM";
     public static WEB_OUT_PUT: string = "WEB_OUT_PUT";
     public static WEB_YEARS: string = "WEB_YEARS";
-    public static TASK_NUM: string = "WEB_TASK";
     public static URL_THREAD_COUNT: string = "URL_THREAD_COUNT";
     public static WEB_THREAD_COUNT: string = "WEB_THREAD_COUNT";
     public static PDF_THREAD_COUNT: string = "PDF_THREAD_COUNT";
@@ -24,20 +23,22 @@ export class HomeComponent implements OnInit {
         webWaitTime: localStorage.getItem(HomeComponent.WEB_WAIT_ITEM) || "1500",
         webOutPut: localStorage.getItem(HomeComponent.WEB_OUT_PUT) || "output/",
         webYears: localStorage.getItem(HomeComponent.WEB_YEARS) || "2",
-        taskNum: localStorage.getItem(HomeComponent.TASK_NUM) || "2",
 
         urlThreadCount: localStorage.getItem(HomeComponent.URL_THREAD_COUNT) || "1",
         webThreadCount: localStorage.getItem(HomeComponent.WEB_THREAD_COUNT) || "2",
         pdfThreadCount: localStorage.getItem(HomeComponent.PDF_THREAD_COUNT) || "1",
     };
 
-    public isShow: boolean = false;
     public minShengList: Array<MinShengEntity> = new Array<MinShengEntity>();
-    public filePath = "";
+    public isShow: boolean = false;
     public isRun: boolean = false;
-    public cursor: number = 0;
+    public filePath: string = "";
 
-    constructor(public router: Router, public ref: ChangeDetectorRef, private zone: NgZone) {
+    private controller: MinShengController = null;
+
+    constructor(public router: Router,
+                public ref: ChangeDetectorRef,
+                private zone: NgZone) {
     }
 
     public onKeyUpWebWaitTime() {
@@ -50,10 +51,6 @@ export class HomeComponent implements OnInit {
 
     public onKeyUpWebYears() {
         localStorage.setItem(HomeComponent.WEB_YEARS, this.info.webYears);
-    }
-
-    public onKeyUpTaskNum() {
-        localStorage.setItem(HomeComponent.TASK_NUM, this.info.taskNum);
     }
 
     public onKeyUpUrlThreadCount() {
@@ -73,48 +70,63 @@ export class HomeComponent implements OnInit {
     }
 
     public onClickGetData(): void {
-        if (this.filePath === "") {
+        if (TextUtils.isEmpty(this.filePath)) {
             alert("请先上传 Excel !");
             return;
         }
-        this.createQueueTask();
+        if (TextUtils.isEmpty(this.info.webOutPut)) {
+            alert("文件输出路径不能为空");
+            return;
+        }
+        if (Number(this.info.urlThreadCount) < 1) {
+            alert("接口请求线程数至少设置 1 个");
+            return;
+        }
+        if (Number(this.info.webThreadCount) < 1) {
+            alert("Web解析线程数至少设置 1 个");
+            return;
+        }
+        if (Number(this.info.urlThreadCount) < 1) {
+            alert("PDF生成线程数至少设置 1 个");
+            return;
+        }
+
+        this.controller = this.createQueueTask();
+        this.minShengList = this.controller.getMinShengBackXlsx();
     }
 
     public onClickParseExcel(): void {
         if (this.isRun) {
-            this.minShengController.stop();
+            this.controller.stop();
             this.isRun = false;
         } else {
-            this.minShengController.start();
+            this.controller.start();
             this.isRun = true;
         }
     }
 
-    private minShengController: MinShengController = null;
+    private createQueueTask(): MinShengController {
+        let controller = new MinShengController();
+        controller.setExcelPath(this.filePath);
+        controller.setOutput(this.info.webOutPut);
+        controller.setWaitTime(this.info.webWaitTime);
+        controller.setUrlThreadCount(this.info.urlThreadCount);
+        controller.setWebThreadCount(this.info.webThreadCount);
+        controller.setPdfThreadCount(this.info.pdfThreadCount);
 
-    private createQueueTask(): void {
-        this.minShengController = new MinShengController();
-        this.minShengController.setExcelPath(this.filePath);
-        this.minShengController.setOutput(this.info.webOutPut);
-        this.minShengController.setWaitTime(this.info.webWaitTime);
-        this.minShengController.setUrlThreadCount(this.info.urlThreadCount);
-        this.minShengController.setWebThreadCount(this.info.webThreadCount);
-        this.minShengController.setPdfThreadCount(this.info.pdfThreadCount);
-
-        this.minShengController.setYouxinCallback((status: any, data: MinShengEntity) => {
-            Logger.log(TAG, "友信 =+=+: ", status, data);
+        controller.setYouxinCallback((status: any, data: MinShengEntity) => {
             this.zone.run(() => data.youxinStatus = status);
         });
-        this.minShengController.setWebCallback((status: any, data: MinShengEntity) => {
-            Logger.log(TAG, "民生 =+=+: ", status);
+        controller.setWebCallback((status: any, data: MinShengEntity) => {
             this.zone.run(() => data.webStatus = status);
         });
-        this.minShengController.setPdfCallback((status: any, data: MinShengEntity) => {
-            Logger.log(TAG, "PDF =+=+: ", status);
+        controller.setPdfCallback((status: any, data: MinShengEntity) => {
             this.zone.run(() => data.pdfStatus = status);
         });
-
-        this.minShengList = this.minShengController.getMinShengBackXlsx();
+        controller.setFailCallback(((code: string, errMsg: string) => {
+            alert(errMsg);
+        }))
+        return controller;
     }
 
     public ngOnInit(): void {
